b22fbd3e1dbf76061916e22caceada472136ae7a
joseph izang <josephizang@gmail.com>
2021-11-30 00:30:38 +0100

    Add non idempotent ops persistence for walletservice

--- src/SLACowryWise.Domain/Services/WalletService.cs
+++ src/SLACowryWise.Domain/Services/WalletService.cs
@@ -11,51 +11,65 @@ namespace SLACowryWise.Domain.Services
     public class WalletService : IWalletService
     {
         private readonly IHttpService _service;
+        private readonly ICreateWallet _createWallet;
+        private readonly IFundsWalletTransfer _walletTransfer;
 
-        public WalletService(IHttpService service)
+        public WalletService(IHttpService service, ICreateWallet createWallet, IFundsWalletTransfer walletTransfer)
         {
             _service = service;
+            _createWallet = createWallet;
+            _walletTransfer = walletTransfer;
         }
         public async Task<WalletPaginatedDtoRoot> GetAllWallets(string page = "1", string pageSize = "10")
         {
-                IRestRequest request = new RestRequest("/api/v1/wallets", Method.GET);
-                request.AddParameter("page", page);
-                request.AddParameter("page_count", pageSize);
-                var client = await _service.InitializeClient().ConfigureAwait(false);
-                var result = await client.ExecuteAsync<WalletPaginatedDtoRoot>(request)
-                    .ConfigureAwait(false);
-                return result.Data;
+            IRestRequest request = new RestRequest("/api/v1/wallets", Method.GET);
+            request.AddParameter("page", page);
+            request.AddParameter("page_count", pageSize);
+            var client = await _service.InitializeClient().ConfigureAwait(false);
+            var result = await client.ExecuteAsync<WalletPaginatedDtoRoot>(request)
+                .ConfigureAwait(false);
+            return result.Data;
         }
 
         public async Task<CreateWalletResponseDto> CreateWallet(CreateWalletInputModel inputModel)
         {
-                IRestRequest request = new RestRequest("/api/v1/wallets", Method.POST);
-                request.AddParameter("account_id", inputModel.AccountId, ParameterType.GetOrPost);
-                request.AddParameter("currency_code", inputModel.CurrencyCode, ParameterType.GetOrPost);
-                var client = await _service.InitializeClient().ConfigureAwait(false);
-                var result = await client.ExecuteAsync<CreateWalletResponseDto>(request)
-                    .ConfigureAwait(false);
-                return result.Data;
+            IRestRequest request = new RestRequest("/api/v1/wallets", Method.POST);
+            request.AddParameter("account_id", inputModel.AccountId, ParameterType.GetOrPost);
+            request.AddParameter("currency_code", inputModel.CurrencyCode, ParameterType.GetOrPost);
+            var client = await _service.InitializeClient().ConfigureAwait(false);
+            var result = await client.ExecuteAsync<CreateWalletResponseDto>(request)
+                .ConfigureAwait(false);
+            var created = new CreateWallet
+            {
+                CreateWalletResponseDto = result.Data
+            };
+            await _createWallet.CreateOneAsync(created).ConfigureAwait(false);
+            return result.Data;
         }
 
         public async Task<SingleWalletResponse> GetAWallet(string walletId)
         {
-                IRestRequest request = new RestRequest($"/api/v1/wallets/{walletId}", Method.GET);
-                var client = await _service.InitializeClient().ConfigureAwait(false);
-                var result = await client.ExecuteAsync<SingleWalletResponse>(request)
-                    .ConfigureAwait(false);
-                return result.Data;
+            IRestRequest request = new RestRequest($"/api/v1/wallets/{walletId}", Method.GET);
+            var client = await _service.InitializeClient().ConfigureAwait(false);
+            var result = await client.ExecuteAsync<SingleWalletResponse>(request)
+                .ConfigureAwait(false);
+            return result.Data;
         }
 
         public async Task<WalletTransferDtoRoot> TransferFundsFromWallet(WalletTransferInputModel inputModel)
         {
-                IRestRequest request = new RestRequest($"/api/v1/wallets/{inputModel.AccountId}", Method.POST);
-                request.AddParameter("product_code", inputModel.ProductCode, ParameterType.GetOrPost);
-                request.AddParameter("amount", inputModel.Amount, ParameterType.GetOrPost);
-                var client = await _service.InitializeClient().ConfigureAwait(false);
-                var result = await client.ExecuteAsync<WalletTransferDtoRoot>(request)
-                    .ConfigureAwait(false);
-                return result.Data;
+            IRestRequest request = new RestRequest($"/api/v1/wallets/{inputModel.AccountId}", Method.POST);
+            request.AddParameter("product_code", inputModel.ProductCode, ParameterType.GetOrPost);
+            request.AddParameter("amount", inputModel.Amount, ParameterType.GetOrPost);
+            var client = await _service.InitializeClient().ConfigureAwait(false);
+            var result = await client.ExecuteAsync<WalletTransferDtoRoot>(request)
+                .ConfigureAwait(false);
+            var transferred = new FundsWalletTransfer
+            {
+                TransferFundsFromWallet = result.Data
+            };
+            await _walletTransfer.CreateOneAsync(transferred).ConfigureAwait(false);
+            return result.Data;
 
         }
     }
